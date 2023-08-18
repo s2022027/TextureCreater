3d6e702667a53bece487f1888015501af99ee31a
Tsuyoshi Hine <72128878+s2022027@users.noreply.github.com>
2023-08-19 06:20:56 +0900

    Update TexCreater20230630.py

--- Tests/Assets/TexCreater20230630.py
+++ Tests/Assets/TexCreater20230630.py
@@ -8,10 +8,9 @@ import sys
 import os
 import clr
 clr.AddReference('UnityEngine')
-#from AN import Tex #?
 import UnityEngine
 from UnityEngine import *
-#import unity_python.client.unity_client as unity_client
+
 
 f = open('Assets/Resources/FileName.txt','r',encoding='UTF-8')
 data = f.read()
@@ -19,26 +18,19 @@ animal = str(data)
 UnityEngine.Debug.Log(type(animal))
 UnityEngine.Debug.Log(animal)
 
-
-#
 path = "img"
 files = os.listdir(path)
-print(files)
 
-
-#
-#animal = "kirin"
-print("Name:"+animal)
 
 input_path = 'img/'+animal+'.jpg'
 output_path = 'mask/'+animal+'_output.jpg'
 
-# 
+
 if not os.path.isfile(input_path):
     print("File not found")
     sys.exit()
 
-# 
+
 input = cv2.imread(input_path)
 output = remove(input)
 cv2.imwrite(output_path, output)
@@ -48,13 +40,11 @@ def imshow(img):
     ret, encoded = cv2.imencode(".jpg", img)
     display(Image(encoded))
 
-# 
+
 bcbk = output_path
-#
-# 
+
+ 
 bcbk = cv2.imread(bcbk)
-
-
 
 
 hsv = cv2.cvtColor(bcbk, cv2.COLOR_BGR2HSV)
@@ -63,37 +53,16 @@ hsv = cv2.cvtColor(bcbk, cv2.COLOR_BGR2HSV)
 bin_img = cv2.inRange(hsv, (0, 10, 0), (255, 255, 255))
 
 
-
-###
 mu = cv2.moments(bin_img, False)
 x,y= int(mu["m10"]/mu["m00"]) , int(mu["m01"]/mu["m00"])
 
 
-###
-#
-#tex_img = bcbk[y - 50 : y + 50, x - 80: x + 80]
 tex_img = bcbk[y - 50 : y + 50, x - 50: x + 50]
-#tex_img = bcbk[y - 170 : y + 20, x - 50: x + 150]
 
-#tex_img = bcbk[y - bcbk.shape[1]/12 : y + bcbk.shape[1]/12, x - bcbk.shape[0]/12: x + bcbk.shape[0]/12]
 
-# 
 cv2.imwrite("Assets/Resources/"+animal+"_texture_test.jpg", tex_img)
-#cv2.imwrite("texture/"+animal+"_texture_test.jpg", tex_img)
 
-
-print("complete!")
-
-#filename = str(file)
-#filename = UnityEditor.gameObject.GetComponent<EnterText>()
-#filename = inputf.GetComponent<InputField>().text
-#filename = UnityEditor.inputField.GetComponent<InputField>()
-
-#UnityEngine.Debug.Log(UnityEditor.GetInput.file)
-#UnityEngine.Debug.Log(str(sys.argv))
-#UnityEngine.Debug.Log(filename)
 
 UnityEngine.Debug.Log("Complete!")
-#UnityEngine.Debug.Log(Tex(num)) #?
-#UnityEngine.Debug.Log(num)
+
 f.close()
