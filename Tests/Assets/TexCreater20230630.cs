0ee819ecbd2847e02287178f73aace78a193619d
Tsuyoshi Hine <72128878+s2022027@users.noreply.github.com>
2023-08-19 06:18:00 +0900

    Update TexCreater20230630.cs

--- Tests/Assets/TexCreater20230630.cs
+++ Tests/Assets/TexCreater20230630.cs
@@ -11,17 +11,7 @@ public class TexCreater20230630 : MonoBehaviour
 {
     public void CreateTexture()
     {
-        //public static dynamic File;
-        //File = GetInput.input;
-        //public class MenuItem_TexCreater20230630_Class
-        //{
-            //[MenuItem("Python Scripts/TexCreater20230630")]
-            //public void TexCreater20230630()
-            //{
-        //int num = 1 ;
         PythonRunner.RunFile("Assets/TexCreater20230630.py");
-        //UnityEngine.Debug.Log(num);
-            //}
-        //}    
+           
     }
 }
